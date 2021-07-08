bc5b5e9aae5749891e1e592125ac212aa474cf1e
Satvik Bansal <54990847+Satvik-B@users.noreply.github.com>
2021-07-08 15:34:50 +0530

    Add files via upload

---
+++ 19CS10053_Assignment5/Source/Gender.cpp
@@ -0,0 +1,8 @@
+// Name : Satvik Bansal
+// Roll No. : 19CS10053
+
+#include <string>
+using namespace std;
+#include "Gender.h"
+const string Male::sName = "Male";
+const string Female::sName = "Female";
